b55770412f04c7b474cfcaebb5ea6ab14aeb8809
Carl Bondesson <carlbondesson92@gmail.com>
2019-10-06 21:43:36 +0200

    first website set-up test

---
+++ react-client/src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        Petify test page
+      </div>
+    )
+  }
+}
+
+export default HomePage;
